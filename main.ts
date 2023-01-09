937bb27a0ff7896a93db99073fae944802d54bfd
PAPADOPOULOS KONSTANTINOS <kospaps@hotmail.com>
2023-01-09 16:31:13 +0200

    minor

--- main.ts
+++ main.ts
@@ -1,11 +1,13 @@
 input.onButtonPressed(Button.A, function () {
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Triangle, 5000, 0, 255, 0, 100, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
     Bird.change(LedSpriteProperty.Y, -1)
 })
 input.onButtonPressed(Button.B, function () {
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Triangle, 5000, 0, 255, 0, 100, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
     Bird.change(LedSpriteProperty.Y, 1)
 })
 let Bird: game.LedSprite = null
 let Obstacles: number[] = []
 Bird = game.createSprite(0, 2)
-Bird.set(LedSpriteProperty.Blink, 150)
-Bird.set(LedSpriteProperty.Brightness, 150)
+Bird.set(LedSpriteProperty.Blink, 130)
+Bird.set(LedSpriteProperty.Brightness, 100)
