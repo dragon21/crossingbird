b1e45a01542ee34b50cee9a9773bb3c5432c0ed5
PAPADOPOULOS KONSTANTINOS <kospaps@hotmail.com>
2023-01-09 16:29:58 +0200

    controls

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
-
+input.onButtonPressed(Button.A, function () {
+    Bird.change(LedSpriteProperty.Y, -1)
+})
+input.onButtonPressed(Button.B, function () {
+    Bird.change(LedSpriteProperty.Y, 1)
+})
+let Bird: game.LedSprite = null
+let Obstacles: number[] = []
+Bird = game.createSprite(0, 2)
+Bird.set(LedSpriteProperty.Blink, 150)
+Bird.set(LedSpriteProperty.Brightness, 150)
